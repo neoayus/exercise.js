106aee4a0fd81aaffbe48184da249a32e54604ea
neoayus <ayus43@outlook.com>
2025-04-16 23:10:11 +0530

    chore: separate dir. for 'dom'

---
+++ webDev-n-DOM/dom/dom08.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program to remove items from a drop-down list.
+
+function removecolor(){
+    var x = document.querySelector("#colorSelect");
+    x.remove(x.selectedIndex); 
+}
+
