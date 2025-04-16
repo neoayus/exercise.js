2c333f4c8d6740c1fb739538aa801c78068edaf8
neoayus <ayus43@outlook.com>
2025-04-16 21:46:12 +0530

    update: new logic to count row nd columns

--- webDev-n-DOM/dom07.js
+++ webDev-n-DOM/dom07.js
@@ -5,15 +5,20 @@ function createTable(){
     var col = parseInt(window.prompt("Enter Column Number :")); 
     var table = document.querySelector("#myTable"); 
     
+    var rowCount = 1 ; 
     for(var x = 0 ; x < row ; x++){
         var newRow = document.createElement("tr");
-
      // creating table column's/ data ; 
+        var colCount = 1 ; 
         for(var y = 0 ; y < col ; y++){
             var newCol = document.createElement("td"); 
-            newCol.innerHTML = ("saxx");
+            newCol.innerHTML = (`Row ${rowCount} Column ${colCount}`);
             newRow.appendChild(newCol) ; 
+
+            colCount++ ; // increment col number 
         }
+        rowCount++ ; // increment row number 
+
         table.appendChild(newRow);
     }
 }
