ca4f361b9abec35747e5a6c445ba2668870a1c99
neoayus <ayus43@outlook.com>
2025-04-22 23:35:06 +0530

    highlight <b> inside a paragraph on mouse hover

--- webDev-n-DOM/dom/dom12.js
+++ webDev-n-DOM/dom/dom12.js
@@ -1,7 +1,24 @@
 // Write a JavaScript program to highlight the bold words of the following paragraph, on mouse over a certain link.
 // Sample link and text :
 
+var boldItems ; 
+window.onload = ()=>{
+    boldItems = document.getElementsByTagName('b'); 
+};
+
+function highlight(){
+    for(var x=0 ; x<boldItems.length ; x++){
+        boldItems[x].style.color = "green";
+    }
+}
+
 var para = document.getElementsByTagName('p')[0] ; 
 para.addEventListener('mouseover', ()=>{
-    document.getElementsByTagName('b') ; 
+    highlight();
 })
+
+para.addEventListener('mouseleave', ()=>{
+    for(var x=0 ; x<boldItems.length ; x++){
+        boldItems[x].style.color = "black";
+    }
+}) 
