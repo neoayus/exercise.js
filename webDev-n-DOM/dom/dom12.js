b24934c0d3d0eaf14010cb40202c39f0e2f6b191
neoayus <ayus43@outlook.com>
2025-04-22 23:10:55 +0530

    move file to ./dom folder

---
+++ webDev-n-DOM/dom/dom12.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program to highlight the bold words of the following paragraph, on mouse over a certain link.
+// Sample link and text :
+
+var para = document.getElementsByTagName('p')[0] ; 
+para.addEventListener('mouseover', ()=>{
+    document.getElementsByTagName('b') ; 
+})
